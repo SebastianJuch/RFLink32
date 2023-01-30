43350f5fd8c7ef13bd26f668a30b4ec4643c34fe
Sebastian Juch <sebastian.juch@googlemail.com>
2023-01-30 20:14:07 +0100

    update

--- RFLink/Plugins/plugin_018.c
+++ RFLink/Plugins/plugin_018.c
@@ -26,23 +26,75 @@ boolean Plugin_018(byte function, const  char *string)
 {
     const  int VONDOM_MinPulses = 450;
     const  int VONDOM_MaxPulses = 600;
-
+    int i;
     //if (RawSignal.Number >= VONDOM_MinPulses && RawSignal.Number <= VONDOM_MaxPulses)
     if (RawSignal.Number >= VONDOM_PulseCount_Button_1)
     {
-        Serial.print("Found 160 Pulses \n");
+        unsigned long duration = ((unsigned long)RawSignal.Pulses[1])*RawSignal.Multiply;
+        Serial.print(RawSignal.Number);
+        Serial.print(" Pulses found \n");
+        Serial.print("RawSignal Multiply ");
+        Serial.print(RawSignal.Multiply);
+        Serial.print("\n");
+        Serial.print("Duration");
+        Serial.print(duration);
+        Serial.print("\n");
+        for (i = 1; i < RawSignal.Number + 1; i++)
+        {
+            bool static start_found = false;
+            bool static end_found = false;
+            int static pulse_counter = 0; 
+            
+
+            unsigned long duration = ((unsigned long)RawSignal.Pulses[i])*RawSignal.Multiply;   
+            
+            if(duration >= PLUGIN_018_PREAMBLE_MIN || duration <= PLUGIN_018_PREAMBLE_MAX)
+            {
+                if(start_found)
+                {
+                   start_found = false;
+                   end_found = true;
+                }
+                Serial.print("Start Found at position ");
+                Serial.print(i);
+                Serial.print("\n");
+                Serial.print(RawSignal.Pulses[i]);
+
+                start_found = true;
+            }
+            else if(start_found)
+            {
+                pulse_counter ++;
+            }
+            else if(end_found)
+            {
+                Serial.print(pulse_counter);
+                Serial.print("Pulses Found between start and stop ");
+                end_found = false;
+                pulse_counter = 0;
+
+            }
+
+
+
+            RFLink::sendRawPrint(RawSignal.Pulses[i] * RawSignal.Multiply);
+            if (i < RawSignal.Number)
+               RFLink::sendRawPrint(',');
+        }
         
+        
+        /*
         unsigned long duration = ((unsigned long)RawSignal.Pulses[1])*RawSignal.Multiply;
         if(duration <= PLUGIN_018_PREAMBLE_MIN || duration >= PLUGIN_018_PREAMBLE_MAX) 
             {
-                #ifdef PLUGIN_018_DEBUG
+                    #ifdef PLUGIN_018_DEBUG
                     Serial.print("failed on check2 duration=");
                     Serial.print(RawSignal.Pulses[1]);
                     Serial.print("  multiply=");Serial.println(duration);
                 #endif
             return false;
             }
-
+*/
         //const int VONDOM_StartPulseDuration = 4200 / RawSignal.Multiply;
         //const int VONDOM_LongPulseMinDuration = 900 / RawSignal.Multiply;
         //const int VONDOM_LongPulseMaxDuration = 1400 / RawSignal.Multiply;
